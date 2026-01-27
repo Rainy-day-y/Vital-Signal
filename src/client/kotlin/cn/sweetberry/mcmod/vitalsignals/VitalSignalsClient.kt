f395db358faba5c5554d2a44e78226543db8e636
Rainy-day-y <rainy.daily@outlook.com>
2026-01-27 10:51:16 +0800

    refactor: Remove unused client code

--- src/client/kotlin/cn/sweetberry/mcmod/vitalsignals/VitalSignalsClient.kt
+++
@@ -1,9 +0,0 @@
-package cn.sweetberry.mcmod.vitalsignals
-
-import net.fabricmc.api.ClientModInitializer
-
-object VitalSignalsClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
